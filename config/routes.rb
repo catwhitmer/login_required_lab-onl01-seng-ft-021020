9fea97a3101056bad3cd936c09512738e55c5fa5
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-30 17:36:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
+  get "/secret", to: "secrets#show"
   
 end
